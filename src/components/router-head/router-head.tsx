52c0e061233ca39584aa6e3de13e5d73d23d4495
Jan Jaap <jan.jaap@digimondo.de>
2024-02-25 23:48:06 +0100

    feat: favicon, theme, manifest, etc.

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -20,7 +20,7 @@ export const RouterHead = component$(() => {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" />
       <meta name="apple-mobile-web-app-title" content="fpv-drone.info" />
       <meta name="application-name" content="fpv-drone.info" />
       <meta name="msapplication-TileColor" content="#da532c" />
