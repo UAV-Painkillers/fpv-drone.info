9e1180242fad6998f3c0e0c41ec70433c76cfbd3
Jan Jaap <jan.jaap@digimondo.de>
2024-04-16 17:48:45 +0200

    feat: dark mode

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,20 +1,28 @@
 import type { QwikIntrinsicElements } from "@builder.io/qwik";
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import LogoImage from "./logo.webp?jsx";
+import LogoDarkImage from "./logo_dark.webp?jsx";
 import styles from "./logo.module.css";
 import classNames from "classnames";
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
+import { useDarkmode } from "~/hooks/use-darkmode";
 
 export const Logo = component$<QwikIntrinsicElements["img"]>((props) => {
-  return (
-    <LogoImage
-      width="200px"
-      height="auto"
-      alt="Logo"
-      {...props}
-      class={classNames(styles.logo, props.class)}
-    />
-  );
+  const imageProps = {
+    width: "200px",
+    height: "auto",
+    alt: "Logo",
+    ...props,
+    class: classNames(styles.logo, props.class),
+  };
+
+  const isDarkmode = useDarkmode();
+
+  if (isDarkmode.value) {
+    return <LogoDarkImage {...imageProps} />;
+  }
+
+  return <LogoImage {...imageProps} />;
 });
 
 export const LogoRegistryInformation: RegisteredComponent = {
