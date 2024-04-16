1980f00af4662cc81c23e3da60c424d71cf27afe
Jan Jaap <jan.jaap@digimondo.de>
2024-04-16 13:12:02 +0200

    fix: analytics

--- src/components/tuning-tools/hooks/use-analyze-log.ts
+++ src/components/tuning-tools/hooks/use-analyze-log.ts
@@ -14,6 +14,7 @@ import {
 import { useLocation } from "@builder.io/qwik-city";
 import type { AnalyzerProgress } from "./types";
 import { AnalyzerStepStatus, makeEmptyProgress } from "./types";
+import { track } from "@vercel/analytics";
 
 export enum AnalyzerState {
   LOADING = "loading",
