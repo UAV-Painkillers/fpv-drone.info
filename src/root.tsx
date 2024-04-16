fff645d80b598b0379e731a1ca6c04dbe6262a2e
Jan Jaap <jan.jaap@digimondo.de>
2024-04-16 10:19:18 +0200

    feat: add tracking of amount of logs beeing analyzed

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,4 @@
-import {
-  component$,
-  useContextProvider,
-  useStore,
-  useVisibleTask$,
-} from "@builder.io/qwik";
+import { component$, useContextProvider, useStore } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
 
 import "./normalize.css";
@@ -11,7 +6,6 @@ import "./global.css";
 import { RouterHead } from "./components/router-head/router-head";
 import type { AppContextState } from "./app.ctx";
 import { AppContext } from "./app.ctx";
-import { injectSpeedInsights } from "@vercel/speed-insights";
 
 export default component$(() => {
   const appContextData = useStore<AppContextState>({
@@ -20,11 +14,6 @@ export default component$(() => {
     serviceWorker: undefined,
   });
   useContextProvider(AppContext, appContextData);
-
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(() => {
-    injectSpeedInsights();
-  });
 
   return (
     <QwikCityProvider>
