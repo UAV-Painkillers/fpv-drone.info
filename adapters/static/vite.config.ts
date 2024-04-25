7bb945dc4e03100e1021c9f7968d0e5a9fce5ad6
Jan Jaap <jan.jaap@digimondo.de>
2024-04-26 01:11:28 +0200

    fix: vercel build

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,10 +1,11 @@
 import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
-import { loadEnv } from "vite";
+// TODO: fix so that og image is generated directly in this project and then use dynamic origin
+// import { loadEnv } from "vite";
 
 export default extendConfig(baseConfig, ({mode}) => {
-  const env = {...process.env, ...loadEnv(mode, process.cwd())};
+  // const env = {...process.env, ...loadEnv(mode, process.cwd())};
 
   return {
     build: {
@@ -15,7 +16,8 @@ export default extendConfig(baseConfig, ({mode}) => {
     },
     plugins: [
       staticAdapter({
-        origin: env.VITE_ORIGIN as string | undefined ?? env.VERCEL_BRANCH_URL as string | undefined ?? "https://fpv-drone.info",
+        // origin: env.VITE_ORIGIN as string | undefined ?? env.VERCEL_BRANCH_URL as string | undefined ?? "https://fpv-drone.info",
+        origin: "https://fpv-drone.info",
       }),
     ],
   };
